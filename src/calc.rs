a8651fca4716e7074a70ddf8bc621ae88e477fc4
Jhanarato <jhanarato@gmail.com>
2019-08-31 17:02:53 +0800

    Added a couple more calculations with unit tests.

--- src/calc.rs
+++ src/calc.rs
@@ -22,11 +22,23 @@ impl Calculation {
     pub fn self_consumption(&self) -> f32 {
         self.generation - self.grid_export
     }
+    
+    // Get the total amount of energy consumed both
+    // from the grid and from the inverter in kilowatt/hours.
+    pub fn total_consumption(&self) -> f32 {
+        self.self_consumption() + self.grid_import
+    }
 
     // Get a value between 0 and 1 for self generation
     // as a portion of total generation.
     pub fn percentage_of_generation_self_consumed(&self) -> f32 {
         self.self_consumption() / self.generation * 100.0
+    }
+    
+    // Get the amount of consumed self generated energy as
+    // a percentage of total amount of energy consumed.
+    pub fn percentage_of_total_consumption_self_generated(&self) -> f32 {
+        self.self_consumption() / self.total_consumption() * 100.0
     }
 }
 
@@ -57,5 +69,30 @@ mod tests {
         };
 
         assert_eq!(calc.percentage_of_generation_self_consumed(), 90.0);
+    }
+
+    #[test]
+    fn total_consumption () {
+        let calc = Calculation {
+            days: 1,
+            generation:  30.0,
+            grid_import: 50.0, 
+            grid_export: 3.0, 
+        };
+        
+        assert_eq!(calc.total_consumption(), 77.0)
+
+    }
+
+    #[test]
+    fn percentage_of_total_consumption_self_generated() {
+        let calc = Calculation {
+            days: 1,
+            generation:  30.0,
+            grid_import: 80.0, 
+            grid_export: 10.0, 
+        };
+        
+        assert_eq!(calc.percentage_of_total_consumption_self_generated(), 20.0);
     }
 }
