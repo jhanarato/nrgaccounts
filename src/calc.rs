c5ae1ad965b6b457a6031612b64d693fc69799df
Jhanarato <jhanarato@gmail.com>
2019-08-31 16:46:58 +0800

    A couple of calculations implemented with unit tests.

--- src/calc.rs
+++ src/calc.rs
@@ -1,9 +1,8 @@
 use chrono::Date;
 use chrono::Local;
 
-/// Readings taken from the meter and inverter.
 pub struct Reading {
-    pub date : Date<Local>, 
+    pub date: Date<Local>, 
     pub generation: f32, 
     pub grid_import: f32,
     pub grid_export: f32,
@@ -11,37 +10,52 @@ pub struct Reading {
 
 // A set of calculations for a given period of time.
 pub struct Calculation {
-    result: String,
+    pub days: u32,         // Number of days between readings.
+    pub generation: f32,    
+    pub grid_import: f32,  
+    pub grid_export: f32,
 }
 
-// Given two readings calculate the stats.
-pub fn calculate(first: Reading, second: Reading) -> Calculation {
-    Calculation {
-        result : String::from("We have a result."),
+impl Calculation {
+    // Get the amount of energy delivered by the inverter
+    // used on the premises in kilowatt/hours.
+    pub fn self_consumption(&self) -> f32 {
+        self.generation - self.grid_export
+    }
+
+    // Get a value between 0 and 1 for self generation
+    // as a portion of total generation.
+    pub fn percentage_of_generation_self_consumed(&self) -> f32 {
+        self.self_consumption() / self.generation * 100.0
     }
 }
 
 // Beginning of unit test section.
 #[cfg(test)]
 mod tests {
-    use chrono::{Local, TimeZone};
     use super::*;
 
     #[test]
-    fn calculate_returns_something() {
-        let first = Reading {
-            date: Local.ymd(2001, 1, 1),
-            generation: 10.0,
-            grid_import: 10.0,
-            grid_export: 10.0,
+    fn self_consumption() {
+        let calc = Calculation {
+            days: 1,
+            generation:  10.0,
+            grid_import: 50.0, // Not important for this test.
+            grid_export: 7.0,
         };
-        let second = Reading {
-            date: Local.ymd(2001, 1, 1),
-            generation: 5.0, 
-            grid_import: 5.0,
-            grid_export: 5.0,
+
+        assert_eq!(calc.self_consumption(), 3.0);
+    }
+
+    #[test]
+    fn percentage_of_generation_self_consumed() {
+        let calc = Calculation {
+            days: 1,
+            generation:  30.0,
+            grid_import: 50.0, // Not important for this test.
+            grid_export: 3.0,
         };
-        let calculation = calculate(first, second);
-        assert_eq!(calculation.result, "We have a result.");
+
+        assert_eq!(calc.percentage_of_generation_self_consumed(), 90.0);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate chrono;
+extern crate time;
 
 pub mod calc;
 
