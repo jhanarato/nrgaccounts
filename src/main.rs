0f35111e1cc81f23312f3b9635a17597d5f0f0bc
Jhanarato <jhanarato@gmail.com>
2019-09-18 08:36:51 +0800

    Put all the pieces together, except user input.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use chrono::{ Local, TimeZone };
+use nrgaccounts::calc::calculate;
+use nrgaccounts::readings::{ Reading, find_change };
 
 fn main() {
+    let first_reading = Reading {
+            date: Local.ymd(2019, 7, 18),
+            generation: 76.5,
+            exports: 1.0,
+            imports: 49.0,
+        };
+
+    let second_reading = Reading {
+        date: Local.ymd(2019, 9, 17),
+        generation: 1371.3,
+        exports: 442.0,
+        imports: 3548.0, 
+    };
+
+    let changes = find_change(first_reading, second_reading); 
+    let calculation = calculate(changes);
     
+    println!("{}", calculation);
 }
