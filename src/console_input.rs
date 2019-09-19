d32424e359c3e5bc29a9281954b9657d6621145f
Jhanarato <jhanarato@gmail.com>
2019-09-19 17:31:47 +0800

    Console input works now, though buggy.

--- src/console_input.rs
+++ src/console_input.rs
@@ -1,7 +1,82 @@
 use chrono::{ NaiveDate };
 use std::io;
+use std::io::prelude::*;
+use std::num::ParseFloatError;
+use crate::readings::{ Reading, ReadingPair };
 
-pub fn ask_for_date() -> Result<NaiveDate, &'static str> {
+
+/// Using user input from the console get two
+/// readings, the earlier reading first.
+pub fn get_reading_pair() -> ReadingPair {
+    println!("First reading:");
+    let first = get_reading();
+    println!("Second reading:");
+    let second = get_reading();
+    ReadingPair { first, second }
+}
+
+fn get_reading()-> Reading {
+
+    let date = loop {
+        print!("Date [YYYY-MM-DD]: ");
+        io::stdout().flush().ok().expect("Could not flush!");
+        let date = ask_for_date();
+        match date {
+           Ok(d)=> break d, 
+           Err(_e) => println!("Could not parse date. Try again."), 
+        }
+    };
+
+    let generation = loop {
+        print!("Generation [kWh]: ");
+        io::stdout().flush().ok().expect("Could not flush!");
+        let generation = ask_for_number();
+        match generation {
+            Ok(g) => break g,
+            Err(_e) => println!("Could not parse number, try again."),
+        }
+    };
+
+    let exports = loop {
+        print!("Grid exports [kWh]:  ");
+        io::stdout().flush().ok().expect("Could not flush!");
+        let exports = ask_for_number();
+        match exports {
+            Ok(ex) => break ex,
+            Err(_e) => println!("Could not parse number, try again."),
+        }
+    };
+
+    let imports = loop {
+        print!("Grid imports [kWh]:  ");
+        io::stdout().flush().ok().expect("Could not flush!");
+        let imports= ask_for_number();
+        match imports {
+            Ok(i) => break i,
+            Err(_e) => println!("Could not parse number, try again."),
+        }
+    };
+
+    Reading {
+        date,
+        generation,
+        exports,
+        imports,
+    }
+}
+
+fn ask_for_number() -> Result<f32, ParseFloatError> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            input.pop() // Remove trailing newline.
+        },
+        Err(e) => panic!("Unexpected error {}", e),
+    };
+    input.parse::<f32>() 
+}
+
+fn ask_for_date() -> Result<NaiveDate, &'static str> {
     
     let mut input = String::new(); 
     match io::stdin().read_line(&mut input) {
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,10 @@
-use chrono::NaiveDate;
 use nrgaccounts::calc::calculate;
-use nrgaccounts::readings::{ Reading, find_change };
-use nrgaccounts::console_input::{ask_for_date};
-use std::io::prelude::*;
-use std::io;
+use nrgaccounts::readings::find_change;
+use nrgaccounts::console_input::get_reading_pair;
 
 fn main() {
-    print!("Enter a date [YYYY-MM-DD]: ");
-    io::stdout().flush().ok().expect("Could not flush!");
-
-    let date = ask_for_date();
-    match date {
-       Ok(d)=> println!("{}", d.format("%d/%m/%Y")),
-       Err(e) => panic!("{}", e),
-    }
-
-    let first_reading = Reading {
-            date: NaiveDate::from_ymd(2019, 7, 18),
-            generation: 76.5,
-            exports: 1.0,
-            imports: 49.0,
-        };
-
-    let second_reading = Reading {
-        date: NaiveDate::from_ymd(2019, 9, 17),
-        generation: 1371.3,
-        exports: 442.0,
-        imports: 3548.0, 
-    };
-
-    let changes = find_change(first_reading, second_reading); 
+    let pair = get_reading_pair();
+    let changes = find_change(pair); 
     let calculation = calculate(changes);
     
     println!("{}", calculation);
